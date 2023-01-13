d447016d97e75895777888001e03b7ccfb7d6412
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-01-13 11:46:13 -0300

    probando

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,4 +2,7 @@ describe('Bubble Sort', function(){
     it('handles an empty array', function(){
         expect( bubbleSort([]) ).toEqual( [] );
     });
+    it('sorts an unordered array', function(){
+        expect( bubbleSort([4, 2, 11, 7, 9, 6]) ).toEqual( [2, 4, 6, 7, 9, 11] );
+    });
 });
