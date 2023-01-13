ba29f6e712210ffbd1b1cf6a5fd07e4d85a76904
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-01-13 12:47:36 -0300

    lito bubble

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+function bubbleSort(array) {
+    /* tu codigo acá */
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < (array.length - 1); j++) {
+            array = swap(array, j, j + 1);
+        } 
+    }
+    return array;
+}
+
+
+function swap(arreglo, index1, index2) {
+    /* tu codigo acá */
+    let temp = arreglo[index1];
+    if (arreglo[index1] > arreglo[index2]) {
+        arreglo[index1] = arreglo[index2];
+        arreglo[index2] = temp;
+    }
+    return arreglo;
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -5,4 +5,17 @@ describe('Bubble Sort', function(){
     it('sorts an unordered array', function(){
         expect( bubbleSort([4, 2, 11, 7, 9, 6]) ).toEqual( [2, 4, 6, 7, 9, 11] );
     });
+    it('handles an ordered array', function(){
+        expect( bubbleSort([2, 4, 6, 7, 9, 11]) ).toEqual( [2, 4, 6, 7, 9, 11] );
+    });
+    it('handles an ordered array', function(){
+        expect( bubbleSort([6, 5, 4, 3, 2, 1]) ).toEqual( [1, 2, 3, 4, 5, 6] );
+    });
+    it('check on swap', function () {
+        spyOn(window, 'swap').and.callThrough(); 
+        // callThrough le dice al spy que utilice el método original chocolate['masticar'], 
+        // mientras que `callFake` haría una copia.
+        window.bubbleSort([4, 2, 11, 7, 9, 11]);
+        expect(window.swap.calls.count()).toEqual(30);
+    });
 });
